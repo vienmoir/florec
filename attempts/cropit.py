7896b838c6155159e12f43b71d3b45101e3d91b5
vienmoir <daria.korotayeva@yandex.ru>
2018-08-20 14:05:27 +0200

    first

---
+++ attempts/cropit.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('..\\testim\\tc.jpg',3) #read img
+b,g,r = cv2.split(img)
+img = cv2.merge([r,g,b]) # revert to rgb
+print(img.shape)
+edges = cv2.Canny(img, 200,400) # apply canny
+
+def square_it(grim, orim): # cannied and rgb on input
+	y, x = grim.shape
+	xc = x//2 # saving space
+	yc = y//2
+	if x > y: # horizontal
+		lw = np.sum(grim[0:y, 0:y] == 255) # left square 
+		cw = np.sum(grim[0:y, xc-yc:xc+yc] == 255) #centre
+		rw = np.sum(grim[0:y, x-y:x] == 255) # right
+	else: # vertical
+		lw = np.sum(grim[0:x, 0:x] == 255) #  top
+		cw = np.sum(grim[yc-xc:yc+xc, 0:x] == 255)
+		rw = np.sum(grim[y-x:y, 0:x] == 255) # bottom 
+	if lw > rw:
+		if lw > cw: #looking for the square with max(white pixels)
+			if x > y:
+				return grim[0:y, 0:y], orim[0:y, 0:y]
+			else:
+				return grim[0:x, 0:x], orim[0:x, 0:x]
+		else:
+			if x > y:
+				return grim[0:y, xc-yc:xc+yc], orim[0:y, xc-yc:xc+yc]
+			else:
+				return grim[yc-xc:yc+xc, 0:x], orim[yc-xc:yc+xc, 0:x]
+	elif rw > cw:
+		if x > y:
+			return grim[0:y, x-y:x], orim[0:y, x-y:x]
+		else:
+			return grim[y-x:y, 0:x], orim[y-x:y, 0:x]
+	else:
+		if x > y:
+			return grim[0:y, xc-yc:xc+yc], orim[0:y, xc-yc:xc+yc]
+		else:
+			return grim[yc-xc:yc+xc, 0:x], orim[yc-xc:yc+xc, 0:x]
+
+edcr, orcr = square_it(edges, img) 
+
+plt.subplot(221),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(222),plt.imshow(orcr)
+plt.title('Cropped  Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(223),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(224),plt.imshow(edcr, cmap = 'gray')
+plt.title('Cropped Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
---
+++ attempts/main.py
@@ -0,0 +1,122 @@
+# coding: utf-8
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+from flonet import FloNet
+from cropim import CropIm
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from imutils import paths
+import numpy as np
+#import argparse
+import random
+import pickle
+import cv2
+import os
+
+print('[info] imported everything, yay')
+
+args = {
+    "dataset": "..\\floral",
+    "model": "..\\florec.model",
+    "labelbin": "..\\lb.pickle",
+    "lplot": "..\\lrelu.png",
+    "aplot": "..\\arelu.png"
+}
+
+# the fun part
+EPOCHS = 50
+INIT_LR = 1e-3 # initial learning rate (Adam will handle it later)
+BS = 16 # batch  size
+IMAGE_DIMS = (90, 90, 3)
+
+#initialize data labels
+data = []
+labels = []
+
+print("[info] loading paths...")
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+
+print('[info] loading images...')
+for imagePath in imagePaths:
+    image = CropIm(imagePath)
+   # print(imagePath)
+    image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+    image = img_to_array(image)
+    data.append(image)
+    
+    label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
+print('[info] images collected')
+
+data = np.array(data, dtype = "float") / 255.0
+labels = np.array(labels)
+print("[info] data matrix: {:.2f}MB".format(data.nbytes / (1024 * 1000.0)))
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size = 0.1, 
+                                                  random_state = 42)
+
+aug = ImageDataGenerator(rotation_range=90, width_shift_range = 0.1,
+                       height_shift_range = 0.1, shear_range = 0.2, 
+                       horizontal_flip = True, vertical_flip = True,
+                       fill_mode = "nearest")
+
+print("[info] compiling model...")
+model = FloNet.build(width = IMAGE_DIMS[1], height = IMAGE_DIMS[0],
+                   depth = IMAGE_DIMS[2], classes = len(lb.classes_))
+opt = Adam(lr= INIT_LR, decay = INIT_LR / EPOCHS)
+model.compile(loss = "categorical_crossentropy", optimizer = opt,
+             metrics = ["accuracy"])
+
+
+print("[info] training network...")
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size = BS),
+                       validation_data = (testX, testY),
+                       steps_per_epoch = len(trainX) // BS,
+                       epochs = EPOCHS, verbose = 2)
+#model.save_weights('first_try.h5')
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="test_loss")
+plt.title("Training Loss")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
+plt.legend(loc="upper right")
+plt.show()
+plt.savefig(args["lplot"])
+
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_acc"], label="test_acc")
+plt.title("Training Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Accuracy")
+plt.legend(loc="upper right")
+plt.show()
+plt.savefig(args["aplot"])
+
+# save the model to disks
+#print("[info] serializing network...")
+#model.save(args["model"])
+ 
+# save the label binarizer to disk
+#print("[info] serializing label binarizer...")
+#f = open(args["labelbin"], "wb")
+#f.write(pickle.dumps(lb))
+#f.close()
